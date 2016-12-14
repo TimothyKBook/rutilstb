41668618ca49dc4e9fc556e8edc6e3eac30af4ab
TimothyKBook <TimothyKBook@gmail.com>
2016-12-14 16:54:16 -0500

    Compromised with unpack function...

--- R/unpack.R
+++ R/unpack.R
@@ -1,15 +1,14 @@
 #' Unpacking a list
 #'
-#' Unpacks a list into the parent environment.
+#' Unpacks a list into the global (or selected) environment.
 #'
 #' @param list A list to be unpacked.
 #'
 #' @return No returns (null).
 #' @export
-unpack <- function(mylist) {
+unpack <- function(mylist, env = globalenv()) {
   objects <- names(mylist)
   for (obj in objects) {
-    assign(obj, mylist[[obj]], envir = parent.env(environment()))
+    assign(obj, mylist[[obj]], envir = env)
   }
-  return(NULL)
 }

e06de46dd258e8735ee81ab22226b8020097e74f
TimothyKBook <TimothyKBook@gmail.com>
2016-12-25 14:17:45 -0500

    Successfully implemented keys in isid.R

--- R/isid.R
+++ R/isid.R
@@ -2,10 +2,12 @@
 #'
 #' @param x Vector or dataframe.
 #'
+#' @param keys Combination of variables to check as unique key.
+#'
 #' @return logical indicating whether or not `x` has distinct elements.  If a
 #' dataframe was given, a vector of logicals.
 #' @export
-isid <- function(x) {
+isid <- function(x, keys = NULL) {
   if (is.vector(x)) {
 
     n <- length(x)
@@ -15,7 +17,7 @@ isid <- function(x) {
       return(FALSE)
     }
 
-  } else if (is.data.frame(x)) {
+  } else if (is.data.frame(x) && is.null(keys)) {
 
     out <- lapply(x, function(var) {
       n <- length(var)
@@ -24,6 +26,15 @@ isid <- function(x) {
     })
 
     return(unlist(out))
+
+  } else if (is.data.frame(x) && !is.null(keys)) {
+
+    dat <- x[!duplicated(x[keys]),]
+    if (nrow(dat) == nrow(x)) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
 
   } else {
 

dad2c3e5b1f2bde902e76a8560435f3907b0a914
TimothyKBook <TimothyKBook@gmail.com>
2017-01-07 16:44:44 -0500

    Added vectorization to mdesc and error message

--- R/mdesc.R
+++ R/mdesc.R
@@ -3,27 +3,40 @@
 #' Creates a table that describes where missing values occur
 #' in a given dataframe.
 #'
-#' @param df A dataframe to analyze.
+#' @param df A dataframe or vector to analyze.
 #'
 #' @return An object of type dataframe describing where missing values occur and
 #' at what rate.
 #' @export
 mdesc <- function(df) {
 
-  var_col <- colnames(df)
+  if (is.vector(df)) {
+    missing_col <- sum(is.na(df))
+    total_col <- length(df)
+    perc_missing_col <- missing_col / total_col
 
-  missing_col <- unlist(lapply(df, function(x) sum(is.na(x))))
+    out <- data.frame(
+      'missing' = missing_col,
+      'total' = total_col,
+      'percent_missing' = perc_missing_col
+    )
 
-  total_col <- nrow(df)
+    return(out)
+  } else if (is.data.frame(df)) {
+    var_col <- colnames(df)
+    missing_col <- unlist(lapply(df, function(x) sum(is.na(x))))
+    total_col <- nrow(df)
+    perc_missing_col <- missing_col / total_col
 
-  perc_missing_col <- missing_col / total_col
+    out <- data.frame(
+      'variable' = var_col,
+      'missing' = missing_col,
+      'total' = total_col,
+      'percent_missing' = perc_missing_col
+    )
 
-  out <- data.frame(
-    'variable' = var_col,
-    'missing' = missing_col,
-    'total_col' = total_col,
-    'percent_missing' = perc_missing_col
-  )
-
-  return(out)
+    return(out)
+  } else {
+    stop('Please enter a vector or data.frame!')
+  }
 }

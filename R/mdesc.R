1e56d5c856db41930d78a7941741f8262682b336
TimothyKBook <TimothyKBook@gmail.com>
2017-01-06 19:51:08 -0500

    Added mdesc function.

---
+++ R/mdesc.R
@@ -0,0 +1,29 @@
+#' Missing Value Description Table
+#'
+#' Creates a table that describes where missing values occur
+#' in a given dataframe.
+#'
+#' @param df A dataframe to analyze.
+#'
+#' @return An object of type dataframe describing where missing values occur and
+#' at what rate.
+#' @export
+mdesc <- function(df) {
+
+  var_col <- colnames(df)
+
+  missing_col <- unlist(lapply(df, function(x) sum(is.na(x))))
+
+  total_col <- nrow(df)
+
+  perc_missing_col <- missing_col / total_col
+
+  out <- data.frame(
+    'variable' = var_col,
+    'missing' = missing_col,
+    'total_col' = total_col,
+    'percent_missing' = perc_missing_col
+  )
+
+  return(out)
+}

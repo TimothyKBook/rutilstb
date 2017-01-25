d00212811066699561b34e25a90a578e35520db6
tim.book <tim.book@summitllc.us>
2017-01-25 13:03:42 -0500

    Added loadNamer

--- R/loadNamer.R
+++ R/loadNamer.R
@@ -1,5 +1,17 @@
+#' loadNamer
+#'
+#' Load objects in an .RData and automatically give it a given name.
+#'
+#' @param file_path The .RData file to load in.
+#' @param name The name you'd like the .RData object to be loaded in as.
+#' @param expected_name If you already have an object in the environment with the same name
+#' as the object about to be loaded in, put that character string here.  It will be preserved
+#' while loading in the new object.
+#'
+#' @return
+#'
+#' @export
 loadNamer <- function(file_path, name, expected_name = NULL) {
-  # DISCLAIMER: NOT YET UNIT TESTED!  USE AT OWN PERIL!
   oldenv <- c(ls(), 'oldenv')
 
   if (!is.null(expected_name)) {
@@ -14,6 +26,8 @@ loadNamer <- function(file_path, name, expected_name = NULL) {
   newenv <- ls()
 
   newobj <- setdiff(newenv, oldenv)
+  if (length(newobj) > 1) stop('Please only use an .RData with one object stored!')
+  if (length(newobj) == 0) stop('.RData was empty!')
 
   assign(name, eval(parse(text = newobj)), envir = globalenv())
 
